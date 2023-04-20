1848a4a70ee89cbcf3b223b89fe64bbc59998095
titusss <26855197+titusss@users.noreply.github.com>
2023-04-20 16:59:32 -0700

    Load config

--- train_2_1_unclip.py
+++ train_2_1_unclip.py
@@ -18,53 +18,71 @@ from kandinsky2.train_utils.trainer_2_1_uclip import train_unclip
 from kandinsky2.model.resample import UniformSampler
 from omegaconf import OmegaConf
 import clip
+
+
 def drop_first_layer(path):
     d = {}
     state_dict = torch.load(path)
     for key in state_dict.keys():
-        if key != 'input_blocks.0.0.weight':
+        if key != "input_blocks.0.0.weight":
             d[key] = state_dict[key]
     return d
 
+
 def main():
-    config = OmegaConf.load('')
-    device = config['device']
-    model = create_model(**config['model_config'])
-    diffusion = create_gaussian_diffusion(**config['diffusion_config'])
-    print('start loading')
-    if config['params_path'] != '':
-        if config['drop_first_layer']:
-            model.load_state_dict(drop_first_layer(config['params_path']), strict=False)
+    config = OmegaConf.load("/content/Kandinsky-2/train_configs/config_unclip_2_1.yaml")
+    device = config["device"]
+    model = create_model(**config["model_config"])
+    diffusion = create_gaussian_diffusion(**config["diffusion_config"])
+    print("start loading")
+    if config["params_path"] != "":
+        if config["drop_first_layer"]:
+            model.load_state_dict(drop_first_layer(config["params_path"]), strict=False)
         else:
-            model.load_state_dict(torch.load(config['params_path']))
-    model = freeze_decoder(model, **config['freeze']).to(device)
-    train_loader = create_loader(**config['data']['train'])
-    image_encoder = MOVQ(**config['image_enc_params']["params"]).half()
-    image_encoder.load_state_dict(torch.load(config['image_enc_params']["ckpt_path"]))
+            model.load_state_dict(torch.load(config["params_path"]))
+    model = freeze_decoder(model, **config["freeze"]).to(device)
+    train_loader = create_loader(**config["data"]["train"])
+    image_encoder = MOVQ(**config["image_enc_params"]["params"]).half()
+    image_encoder.load_state_dict(torch.load(config["image_enc_params"]["ckpt_path"]))
     image_encoder = image_encoder.eval().to(device)
     schedule_sampler = UniformSampler(diffusion)
-    text_encoder = TextEncoder(**config['text_enc_params']).eval().half().to(device)
-    optimizer = get_obj_from_str(config['optim_params']["name"])(
-            model.parameters(), **config['optim_params']["params"]
+    text_encoder = TextEncoder(**config["text_enc_params"]).eval().half().to(device)
+    optimizer = get_obj_from_str(config["optim_params"]["name"])(
+        model.parameters(), **config["optim_params"]["params"]
+    )
+    if "scheduler_params" in config:
+        lr_scheduler = get_obj_from_str(config["scheduler_params"]["name"])(
+            optimizer, **config["scheduler_params"]["params"]
         )
-    if 'scheduler_params' in config:
-        lr_scheduler = get_obj_from_str(config['scheduler_params']["name"])(
-                optimizer, **config['scheduler_params']["params"]
-            )
     else:
         lr_scheduler = None
-    clip_model, _ = clip.load(config['clip_name'], device="cpu", jit=False)
+    clip_model, _ = clip.load(config["clip_name"], device="cpu", jit=False)
     clip_model.transformer = None
     clip_model.positional_embedding = None
     clip_model.ln_final = None
     clip_model.token_embedding = None
     clip_model.text_projection = None
     clip_model = clip_model.eval().to(device)
-    train_unclip(unet=model, diffusion=diffusion, image_encoder=image_encoder,
-                  clip_model=clip_model, text_encoder=text_encoder, optimizer=optimizer,
-                  lr_scheduler=lr_scheduler, schedule_sampler=schedule_sampler, 
-                  train_loader=train_loader, val_loader=None, scale=config['image_enc_params']['scale'],
-                  num_epochs=config['num_epochs'], save_every=config['save_every'], save_name=config['save_name'],
-                  save_path=config['save_path'],  inpainting=config['inpainting'], device=device)
-if __name__ == '__main__':
+    train_unclip(
+        unet=model,
+        diffusion=diffusion,
+        image_encoder=image_encoder,
+        clip_model=clip_model,
+        text_encoder=text_encoder,
+        optimizer=optimizer,
+        lr_scheduler=lr_scheduler,
+        schedule_sampler=schedule_sampler,
+        train_loader=train_loader,
+        val_loader=None,
+        scale=config["image_enc_params"]["scale"],
+        num_epochs=config["num_epochs"],
+        save_every=config["save_every"],
+        save_name=config["save_name"],
+        save_path=config["save_path"],
+        inpainting=config["inpainting"],
+        device=device,
+    )
+
+
+if __name__ == "__main__":
     main()
